66070f488900234b52fdfec33061e25fc711da72
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-06-03 04:15:34 -0300

    Fix: Removing extra comma that was breaking sql script.

--- scripts/init.sql
+++ scripts/init.sql
@@ -6,5 +6,5 @@ create table emails (
     id serial not null,
     date timestamp not null default current_timestamp,
     subject varchar(100) not null,
-    message varchar(250) not null,
+    message varchar(250) not null
 );
